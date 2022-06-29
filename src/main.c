124b4dfefa2e819dbe48b0eeea1c0b70b5b90e9a
Aki-7 <aki.develop8128@gmail.com>
2022-06-29 18:21:56 +0900

    simplify

--- src/main.c
+++ src/main.c
@@ -7,11 +7,6 @@ main()
 {
   struct y11_compositor *compositor;
   struct y11_xdg_shell_desktop *desktop;
-  struct y11_data_device_manager *device_manager;
-  struct y11_seat *seat;
-  struct y11_shell *shell;
-  struct y11_zxdg_output_manager *output_manager;
-  struct y11_output *output;
   const char *socket;
 
   compositor = y11_compositor_create();
@@ -25,31 +20,6 @@ main()
   }
 
   wl_display_init_shm(compositor->display);
-
-  device_manager = y11_data_device_manager_create(compositor->display);
-  if (device_manager == NULL) {
-    return 1;  // TODO: Error log
-  }
-
-  seat = y11_seat_create(compositor);
-  if (seat == NULL) {
-    return 1;  // TODO: Error log
-  }
-
-  shell = y11_shell_create(compositor);
-  if (shell == NULL) {
-    return 1;  // TODO: Error log
-  }
-
-  output_manager = y11_zxdg_output_manager_create(compositor);
-  if (output_manager == NULL) {
-    return 1;  // TODO: Error log
-  }
-
-  output = y11_output_create(compositor);
-  if (output == NULL) {
-    return 1;  // TODO: Error log
-  }
 
   socket = wl_display_add_socket_auto(compositor->display);
   if (socket == NULL) {
